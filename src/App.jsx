ca2ef58e4cc68b390a31cdc424396b0d965beeac
InsureGateWay <gatewayinsurepro@gmail.com>
2026-08-28 03:56:02 +0100

    Remove technology competencies strip

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,6 @@ import EnterpriseAIService from './components/EnterpriseAIService';
 import Partners from './components/Partners';
 import ProductsShowcase from './components/ProductsShowcase';
 import TrustAndCredentials from './components/TrustAndCredentials';
-import TechnologyCompetencies from './components/TechnologyCompetencies';
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -46,7 +45,6 @@ function HomePage() {
   return (
     <>
       <Hero />
-      <TechnologyCompetencies />
       <WhatWeDo />
       <Industries />
       <WhyZigo />
